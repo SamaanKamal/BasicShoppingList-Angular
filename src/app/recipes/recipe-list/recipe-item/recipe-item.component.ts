e27358240eeaa9032861e11410c35ebe40af982e
Samaan Kamal <samaankamal72@gmail.com>
2024-02-01 15:16:13 +0200

    manging data with services and passing it

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,8 +9,11 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent {
   @Input() recipe!:Recipe
-  @Output() selectedFeature = new EventEmitter();
+
+  constructor(private recipeService:RecipeService){}
+
   onSelect(){
-    this.selectedFeature.emit();
+    // this.selectedFeature.emit();
+    this.recipeService.recipeSelectd.emit(this.recipe);
   }
 }
