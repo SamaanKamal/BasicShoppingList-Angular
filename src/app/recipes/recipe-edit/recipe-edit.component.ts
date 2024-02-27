6dafb27fff489503b04b837fbb5b69a96b23d827
Samaan Kamal <samaankamal72@gmail.com>
2024-02-27 17:39:22 +0200

    added edit recipe code without validation

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,16 +9,57 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit{
-  id?:number;
+  recipeForm!:FormGroup;
+  id!:number;
   editMode:boolean=false
-  constructor(private route :ActivatedRoute){}
+  constructor(private route :ActivatedRoute,private recipeService:RecipeService){}
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params:Params) =>{
         this.id= +params['id'];
         this.editMode =params['id']!= null;
+        this.initForm();
       }
     );
+  }
+
+  onSubmit(){
+
+  }
+
+  private initForm(){
+    let recipeName='';
+    let recipeImagePath='';
+    let recipeDescription='';
+    let recipeIngredients = new FormArray<FormGroup>([]);
+
+    if(this.editMode)
+    {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName=recipe.name;
+      recipeImagePath=recipe.imagePath;
+      recipeDescription=recipe.description;
+      if(recipe['ingredients'])
+      {
+        for(let ingredient of recipe.ingredients){
+          recipeIngredients.push(
+            new FormGroup({
+              'name':new FormControl(ingredient.name),
+              'amount':new FormControl(ingredient.amount),
+            })
+          )
+        }
+      }
+    }
+    this.recipeForm=new FormGroup({
+      name:new FormControl(recipeName),
+      imagePath:new FormControl(recipeImagePath),
+      description:new FormControl(recipeDescription),
+      ingredients: recipeIngredients,
+    });
+  }
+  get controls() { // a getter!
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 }
