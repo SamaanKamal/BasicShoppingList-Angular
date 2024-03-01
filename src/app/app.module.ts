d493b87a2a48d2545727344b141dcf5817ab4f4e
Samaan Kamal <samaankamal72@gmail.com>
2024-03-01 18:58:26 +0200

    setting up a service for http with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,8 +14,6 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
@@ -32,7 +33,7 @@ import { RecipeService } from './recipes/recipe.service';
     RecipeEditComponent
   ],
   imports: [
-    BrowserModule,FormsModule,ReactiveFormsModule,AppRoutingModule
+    BrowserModule,FormsModule,ReactiveFormsModule,HttpClientModule,AppRoutingModule
   ],
   providers: [ShoppingListService,RecipeService],
   bootstrap: [AppComponent]
