5adf86939a5bc0ef1df71e58b5d2e3c69c035728
Samaan Kamal <samaankamal72@gmail.com>
2024-03-04 22:58:00 +0200

    protected authenticated routes

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { map, take } from 'rxjs';
+
+export const authGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return authService.user.pipe(
+    take(1),
+    map((user) => {
+      const isAuthenticated = !!user;
+      if (isAuthenticated) {
+        return true;
+      }
+      return router.createUrlTree(['/auth']);
+    })
+  );
+};
